f5e4babd900733dfa76ddc8da6476e75559b3010
yigitaa <yigitarikok@gmail.com>
2025-07-03 17:52:19 +0300

    data load edit

--- src/data_download.py
+++ src/data_download.py
@@ -1,18 +1,17 @@
 import os
 import pandas as pd
-from kaggle.api.kaggle_api_extended import KaggleApi
+# from kaggle.api.kaggle_api_extended import KaggleApi
 
-# Kaggle config dosyasının yolu
-os.environ['KAGGLE_CONFIG_DIR'] = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config')
-
-def download_telco_data():
-    api = KaggleApi()
-    api.authenticate()
-    # Dataset: https://www.kaggle.com/datasets/blastchar/telco-customer-churn
-    api.dataset_download_files('blastchar/telco-customer-churn',
-                              path=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data'),
-                              unzip=True)
-    print('Veri indirildi ve data/ klasörüne çıkarıldı.')
+# os.environ['KAGGLE_CONFIG_DIR'] = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config')
+#
+# def download_telco_data():
+#     api = KaggleApi()
+#     api.authenticate()
+#     # Dataset: https://www.kaggle.com/datasets/blastchar/telco-customer-churn
+#     api.dataset_download_files('blastchar/telco-customer-churn',
+#                               path=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data'),
+#                               unzip=True)
+#     print('Veri indirildi ve data/ klasörüne çıkarıldı.')
 
 def load_telco_data():
     data_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'WA_Fn-UseC_-Telco-Customer-Churn.csv')
